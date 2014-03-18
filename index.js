6bce955af1f77f480ce758537830064a9195eaa4
Matt Edelman <medelman@paypal.com>
2014-03-18 15:19:40 -0700

    adding database connectivity

--- index.js
+++ index.js
@@ -4,11 +4,17 @@
 var kraken = require('kraken-js'),
     app = require('express')(),
     options = require('./lib/spec')(app),
+    db = require ('./lib/database'),
     port = process.env.PORT || 8000;
 
 
-app.use(kraken(options));
-
+app.use(kraken({
+    "onconfig": function(settings, cb) {
+        var dbConfig = settings.get("databaseConfig");
+        db.config(dbConfig);
+        cb(null, settings);
+    }
+}));
 
 app.listen(port, function (err) {
     console.log('[%s] Listening on http://localhost:%d', app.settings.env, port);
