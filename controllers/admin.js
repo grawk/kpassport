81facc8ae2c17ee21613023cd83ac23df405fd67
Matt Edelman <medelman@paypal.com>
2014-03-19 17:00:33 -0700

    adding middleware configuration, profile/admin page security

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,18 +1,17 @@
 'use strict';
 
 
-var AdminModel = require('../models/admin');
+var AdminModel = require('../models/admin'),
+    auth = require('../lib/auth');
 
 
-module.exports = function (app) {
+module.exports = function(app) {
 
     var model = new AdminModel();
 
-
-    app.get('/admin', function (req, res) {
-        
+    app.get('/admin', auth.isAuthenticated('admin'), function(req, res) {
+        //console.log("req.user", req.user);
         res.render('admin', model);
-        
     });
 
 };
--- index.js
+++ index.js
@@ -7,12 +7,15 @@ var kraken = require('kraken-js'),
     db = require('./lib/database'),
     crypto = require('./lib/crypto'),
     User = require('./models/user'),
-
+    passport = require('passport'),
+    auth = require('./lib/auth'),
     port = process.env.PORT || 8000;
+
 
 
 app.use(kraken({
     "onconfig": function(settings, cb) {
+        console.log("app.onconfigure");
         //configure db connection and password crypt difficulty
         var dbConfig = settings.get("databaseConfig"),
             cryptConfig = settings.get("bcrypt");
@@ -23,23 +26,47 @@ app.use(kraken({
         var u1 = new User({
             name: 'Kraken McSquid',
             login: 'kraken',
-            password: 'releaseMe',
+            password: 'kraken',
             role: 'admin'
         });
 
         var u2 = new User({
             name: 'Ash Williams',
-            login: 'awilliams',
-            password: 'boomstick',
+            login: 'ash',
+            password: 'ash',
             role: 'user'
         });
 
         //Ignore errors. In this case, the errors will be for duplicate keys as we run this app more than once.
         u1.save();
         u2.save();
+
+        //Tell passport to use our newly created local strategy for authentication
+        passport.use(auth.localStrategy());
+
+        //Give passport a way to serialize and deserialize a user. In this case, by the user's id.
+        passport.serializeUser(function(user, done) {
+            done(null, user.id);
+        });
+
+        passport.deserializeUser(function(id, done) {
+            User.findOne({
+                _id: id
+            }, function(err, user) {
+                done(null, user);
+            });
+        });
+
         cb(null, settings);
     }
 }));
+
+app.on('middleware:before', function(eventargs) {
+    if (eventargs.config.name === "router") {
+        app.use(passport.initialize());
+        app.use(passport.session());
+    }
+});
 
 app.listen(port, function(err) {
     console.log('[%s] Listening on http://localhost:%d', app.settings.env, port);
